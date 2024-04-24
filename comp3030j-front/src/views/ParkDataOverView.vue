65fbd296ccee93aed445cdbd481be482430f4fde
Sandoleathy <3296446003@qq.com>
2024-04-24 23:02:43 +0800

    给用户展示园区信息页面，以及深入研究了chart.js如何使用

---
+++ comp3030j-front/src/views/ParkDataOverView.vue
@@ -0,0 +1,20 @@
+<template>
+    <!--园区信息展示-用户-->
+    <el-container class="container">
+        <el-main>
+            <EnergyOverviewChart></EnergyOverviewChart>
+        </el-main>
+    </el-container>
+</template>
+<script setup>
+import EnergyOverviewChart from "/src/components/EnergyOverviewChart.vue"
+
+</script>
+<style scoped>
+.container {
+   top:0;
+    left: 0;
+    position: absolute;
+    min-width: 100vw; /* 根据实际内容调整 */
+}
+</style>
