220770f450113dfe993fc141420547f4af505a30
unknown <ckr_mm_ckr@163.com>
2024-05-11 15:12:36 +0800

    login first letter capitalize

--- comp3030j-front/src/router/index.ts
+++ comp3030j-front/src/router/index.ts
@@ -36,7 +36,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/login.vue')
+      component: () => import('../views/Login.vue')
     },
     {
       path: '/register',
