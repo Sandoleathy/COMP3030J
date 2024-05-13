052c3d4fa9296aa00876015df081f79ae2f37ea9
蓑衣带雨 <1459150273@qq.com>
2024-05-14 02:10:49 +0800

    更正代理路径在ruoyi中的设置

--- comp3030j-front/vite.config.ts
+++ comp3030j-front/vite.config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
   server: {
     port: 5173, // 开发服务器端口
     proxy: {
-      '/api': {
+      '/villa/api': {
         //所有向后端项目的请求都直接使用/api，不要用localhost:8080，否则会触发跨域请求报错
         target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '') // 路径重写规则
+        rewrite: path => path.replace(/^\/villa\/api/, '') // 路径重写规则
       }
     }
   },
