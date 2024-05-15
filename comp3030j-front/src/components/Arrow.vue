10745546ceb139f42f6f0ec099b17794e8c7c6c0
Sandoleathy <3296446003@qq.com>
2024-05-15 19:09:17 +0800

    增加了风向标文件，为园区总览增加了天气数据卡片

---
+++ comp3030j-front/src/components/Arrow.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wind-vane">
+        <table style="width: 100%;height: 100%;" >
+            <tr>
+                <td></td>
+                <td style="vertical-align: top;">North</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td style="text-align: left;">West</td>
+                <td></td>
+                <td style="text-align: right;">East</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td style="vertical-align: bottom;">South</td>
+                <td></td>
+            </tr>
+        </table>
+        <div :style="{ transform: `rotate(${degree}deg)` }" class="arrow"></div>
+    </div>
+    
+  </template>
+  
+<script setup>
+const props = defineProps({
+    degree: Number
+})
+</script>
+  
+<style>
+.arrow {
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent; /* 左侧透明边框 */
+    border-right: 10px solid transparent; /* 右侧透明边框 */
+    border-top: 40px solid rgb(91, 91, 91); /* 底部实线边框，可以根据需要设置颜色 */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+}
+.wind-vane {
+      width: 120px;
+      height: 120px;
+      border: 3px solid gray;
+      border-radius: 50%; /* 圆形形状 */
+      position: relative;
+      overflow: hidden;
+    }
+</style>
+  
