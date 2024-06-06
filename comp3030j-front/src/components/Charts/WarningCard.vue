a76110a205dbabbbbddaa2f75e599686bd0990b5
Sandoleathy <3296446003@qq.com>
2024-06-06 10:15:09 +0800

    删除了模拟数据

--- comp3030j-front/src/components/Charts/WarningCard.vue
+++ comp3030j-front/src/components/Charts/WarningCard.vue
@@ -63,10 +63,10 @@ const addWarningNotice = (index) => {
     status: "0"
   };
   addNotice(form).then(response => {
-    console.log(response.data)
+    // console.log(response.data)
     ElMessage.success(t('weatherInfo.sendSuccessful'))
   }).catch(error => {
-    console.log(error)
+    // console.log(error)
   })
 }
 const getWarningInfo = () => {
@@ -81,26 +81,7 @@ const getWarningInfo = () => {
         warnings.value = data.warning
         updateTime.value = data.updateTime
         //console.log(data)
-
-        //模拟数据
-        warnings.value = [{
-          "id": "10102010020230403103000500681616",
-          "sender": "上海中心气象台",
-          "pubTime": "2023-04-03T10:30+08:00",
-          "title": "上海中心气象台发布大风蓝色预警[Ⅳ级/一般]",
-          "startTime": "2023-04-03T10:30+08:00",
-          "endTime": "2023-04-04T10:30+08:00",
-          "status": "active",
-          "level": "",
-          "severity": "Minor",
-          "severityColor": "Blue",
-          "type": "1006",
-          "typeName": "大风",
-          "urgency": "",
-          "certainty": "",
-          "text": "上海中心气象台2023年04月03日10时30分发布大风蓝色预警[Ⅳ级/一般]：受江淮气旋影响，预计明天傍晚以前本市大部地区将出现6级阵风7-8级的东南大风，沿江沿海地区7级阵风8-9级，请注意防范大风对高空作业、交通出行、设施农业等的不利影响。",
-          "related": ""
-        }]
+      
     }).catch(error => {
         console.log(error)
     })
